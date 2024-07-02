de3bd4aa8dab6da433fbb74e97240c6bb7970d64
gerardjr42 <ggarciajr12@gmail.com>
2024-07-01 20:59:21 -0400

    Set up routes
    1. Installed react-router-dom
    2. Created Components and Pages
    3. Set up paths and elements for each router
    4. Created boilerplate for all Pages and Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { useEffect } from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
+import NavBar from './Components/NavBar'
+import Edit from './Pages/Edit'
+import FourOFour from './Pages/FourOFour'
+import Home from './Pages/Home'
+import Index from './Pages/Index'
+import New from './Pages/New'
+import Show from './Pages/Show'
 
 //Import our local host API and save it to APU variable
 const API = import.meta.env.VITE_API_URL
@@ -16,8 +24,22 @@ function App() {
   }, [])
 
   return (
-    <>
-    </>
+    <div>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path='/' element={ <Home /> } />
+            <Route path='/workouts' element={< Index />} />
+            <Route path='/workouts/:id' element={ <Show />} />
+            <Route path='/workouts/:id/edit' element={ <Edit />} />
+            <Route path='/workouts/new' element={ <New />} />
+            {/* Use "*" to indicate any unknown routes will be redircted to our 404 page */}
+            <Route path='*' element={ <FourOFour />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
   )
 }
 
---
+++ src/Components/WorkoutNewForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WorkoutNewForm = () => {
+  return (
+    <div>WorkoutNewForm</div>
+  )
+}
+
+export default WorkoutNewForm
