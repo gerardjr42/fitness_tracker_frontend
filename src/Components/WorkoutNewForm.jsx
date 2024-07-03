6344d47b31b9fde1841fc99d2818f0f0054805fc
gerardjr42 <ggarciajr12@gmail.com>
2024-07-02 20:02:25 -0400

    New Workout Form
    1. Made call to our backend to add a new workout and navigate to all workouts after its successful
    2. Implemented handleSumbit function

--- src/Components/WorkoutNewForm.jsx
+++ src/Components/WorkoutNewForm.jsx
@@ -1,39 +1,85 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const API = import.meta.env.VITE_API_URL
 
 const WorkoutNewForm = () => {
-  const [workout, setWorkout] = useState({})
+  const [workout, setWorkout] = useState({
+    type: "",
+    durationInMinutes: 0,
+    caloriesBurned: 0,
+    date: ""
+  })
+
+  let navigate = useNavigate()
+
+  const handleTextChange = (e) => {
+    setWorkout({
+      ...workout,
+      [e.target.id]: e.target.value})
+  }
+
+  const addWorkout = () => {
+    fetch(`${API}/workouts`, {
+      method: "POST",
+      // Don't forget to stringify
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(() => {
+      navigate("/workouts")
+    })
+    .catch((e) => console.error(e))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addWorkout()
+    setWorkout({
+      type: "", 
+      durationInMinutes: 0,
+      caloriesBurned: 0,
+      date:""})
+  }
+
   return (
     <div>WorkoutNewForm
-      <form>
-        <label>Type:</label>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='type'>Type:</label>
         <input 
         type="text"
         id="type"
         value={workout.type}
+        onChange={handleTextChange}
         required
         />
 
-        <label>Duration:</label>
+        <label htmlFor='durationInMinutes'>Duration:</label>
         <input 
         type="number"
-        id="duration"
+        id="durationInMinutes"
         value={workout.durationInMinutes}
+        onChange={handleTextChange}
         required
         />
 
-        <label>Calories Burned:</label>
+        <label htmlFor='caloriesBurned'>Calories Burned:</label>
         <input 
         type="number"
-        id="calories"
+        id="caloriesBurned"
         value={workout.caloriesBurned}
+        onChange={handleTextChange}
         required
         />
         
-        <label>Date:</label>
+        <label htmlFor='date'>Date:</label>
         <input 
         type="text"
         id="date"
         value={workout.date}
+        onChange={handleTextChange}
         required
         />
         <button>Submit</button>
